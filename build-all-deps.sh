1a869ec302ffa342d1acb2a9e8153fb659171a24
randy-waterhouse <kiwigb@yahoo.com>
2015-07-07 01:48:56 +1200

    Add build-all-deps.sh script.

---
+++ build-all-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+## Install binaries from repos for linux
+
+sudo apt-get update -qq
+sudo apt-get install -y ssh build-essential libboost-all-dev libssl-dev libsqlite3-dev 
+sudo apt-get install -y qtbase5-dev qt5-default gcc-4.8-plugin-dev
+
+## Build/install deps for linux from src
+
+./deps/build-scripts/install-libcutl.sh
+./deps/build-scripts/install-odb.sh
+./deps/build-scripts/install-libodb.sh
+./deps/build-scripts/install-libodb-sqlite.sh
+./deps/build-scripts/install-libqrencode.sh
