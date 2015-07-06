267700b51243543703b849e7593aa1fd95eeff80
randy-waterhouse <kiwigb@yahoo.com>
2015-07-06 23:26:21 +1200

    Travis tweaks

--- deps/build-scripts/install-odb.sh
+++ deps/build-scripts/install-odb.sh
@@ -2,7 +2,6 @@
 set -ex
 
 ## Build and install ODB compiler for linux from src
-sudo apt-get install gcc-4.8-plugin-dev
 wget http://www.codesynthesis.com/download/odb/2.3/odb-2.3.0.tar.bz2
 tar -xjvf odb-2.3.0.tar.bz2
 cd odb-2.3.0 && ./configure && make
